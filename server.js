31ef84d7883a023136f64996484aac5d7d337e7a
Yuepan Vue <yuepan.vue@gmail.com>
2022-10-10 20:24:46 -0500

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 
 const mongoose = require('mongoose')
@@ -23,6 +26,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT || 3000) 
 
